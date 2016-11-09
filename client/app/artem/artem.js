3fc04d24b58baa63ebe9ada17297ccd4c953dce4
artemu9 <mandarin122@gmail.com>
2016-11-10 00:18:15 +0200

    change ctrl, add styles, add view

--- client/app/artem/artem.js
+++ client/app/artem/artem.js
@@ -6,29 +6,48 @@
     function noteBookCtrl() {
 
         var vm = this;
-       
+        vm.filterBySite = "";
+        vm.sortField = "site";
+        vm.reverse = false;
         vm.diary = [];
         vm.save = save;
+        vm.sort = sort;
         vm.currentInf = null;
         vm.add = add;
+        vm.edit = edit;
     
 
         init();
 
         function save() {
             console.log("Button Save clicked");
-            vm.diary += vm.currentInf;
-            
+            if(vm.currentInf.index === vm.diary.length) {
+               vm.diary.push(vm.currentInf);
+            } else {
+                vm.diary[vm.currentInf.index] = vm.currentInf;
+            } 
         }
         function init() {
             vm.diary = [
-                { site: 'vk', login: 'mylogin', pass: 'mypass' },
-                { site: 'inst', login: 'login', pass: 'pass' },
+                { index:0, site: 'vk', login: 'mylogin', pass: 'mypass' },
+                {  index:1, site: 'inst', login: '1login', pass: '1pass' },
             ];
         }
          function add() {
             console.log("Button add clicked");
-            vm.currentInf = { site: '', login: '', pass: '' };
+            vm.currentInf = { index:vm.diary.length, site: '', login: '', pass: '' };
+        }
+         function edit(row) {
+            console.log("Row was clicked", row);
+            vm.currentInf = angular.copy(row);
+        }
+        function sort(fieldName) {
+            if (vm.sortField === fieldName) {
+                vm.reverse = !vm.reverse;
+            } else {
+                vm.sortField = fieldName;
+                vm.reverse = false;
+            }
         }
          
 }})();
