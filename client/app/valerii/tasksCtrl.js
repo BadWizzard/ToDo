2e8dafc1b93a6ebd6d16aaeb085bad9f2ebf904e
Regina90 <parkerson@bk.ru>
2016-11-19 22:51:22 +0200

    renamed files, changed popUp

--- client/app/valerii/tasksCtrl.js
+++ client/app/valerii/tasksCtrl.js
@@ -14,11 +14,14 @@
         td.popup = "";
         td.reverse = false;
         td.currentInf = null;
+        td.butCheck = false;
+        td.check = false;
         td.save = save;
         td.sort = sort;
         td.add = add;
         td.edit = edit;
         td.delete = delet;
+        td.valid = validation;
 
         init();
 
@@ -35,31 +38,43 @@
             }];
 
             td.rows = [{
-                index:0,
+                index: 0,
                 task: 'Поспать',
-                importance: td.import[0].text,
-                date: '2015-08-08 7:30',
-            }, {
-                index:1,
-                task: 'Покушать',
                 importance: td.import[1].text,
-                date: '2015-06-18 15:30',
+                date: new Date("2016-11-10"),
+                check: false
+            }, {
+                index: 1,
+                task: 'Покушать',
+                importance: td.import[0].text,
+                date: new Date("2015-11-10"),
+                check: false
             }];
         }
 
         function save() {
             console.log("Button Save clicked");
-            if(td.currentInf.index === td.rows.length) {
-               td.rows.push(td.currentInf);
+            if (td.currentInf.date.length == 0) {
+                td.currentInf.date = new Date();
+            }
+            if (td.currentInf.importance.length == 0) {
+                td.currentInf.importance = td.import[1].text;
+            }
+            if (td.currentInf.index === td.rows.length) {
+                td.rows.push(td.currentInf);
             } else {
                 td.rows[td.currentInf.index] = td.currentInf;
-            } 
+            }
         }
 
-        function add () {
+        function validation(task) {
+            td.butCheck = (task.length != 0);
+        }
+
+        function add() {
             console.log('add');
             td.popup = "ДОБАВИТЬ";
-            td.currentInf = { index:td.rows.length, task: '', importance: '', date: '' };
+            td.currentInf = { index: td.rows.length, task: '', importance: '', date: '' };
         }
 
         function edit(row) {
@@ -70,12 +85,13 @@
 
         function delet(row) {
             console.log("delete");
-            for(var i = 0; i < td.rows.length; i++) {
-                if(row.index == td.rows[i].index) {
-                    td.rows.splice(i,1);
+            for (var i = 0; i < td.rows.length; i++) {
+                if (row.index == td.rows[i].index) {
+                    td.rows.splice(i, 1);
                 }
             }
         }
+
         function sort(fieldName) {
             td.reverse = !td.reverse;
             td.sortField = fieldName;
