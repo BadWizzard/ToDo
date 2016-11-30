8905b778f14ac7419a02f7e40f939e74e663fc2e
artemu9 <mandarin122@gmail.com>
2016-11-30 14:09:16 +0200

    add styles and etc.

--- client/app/artem/artem.js
+++ client/app/artem/artem.js
@@ -1,9 +1,9 @@
-(function() {
+(function () {
 
     var moduleId = "noteBookCtrl";
-    angular.module("ToDo").controller(moduleId, [noteBookCtrl]);
+    angular.module("ToDo").controller(moduleId, ['$http', noteBookCtrl]);
 
-    function noteBookCtrl() {
+    function noteBookCtrl($http) {
 
         var vm = this;
         vm.filterBySite = "";
@@ -15,32 +15,65 @@
         vm.currentInf = null;
         vm.add = add;
         vm.edit = edit;
-    
-
+        
         init();
 
         function save() {
             console.log("Button Save clicked");
-            if(vm.currentInf.index === vm.diary.length) {
-               vm.diary.push(vm.currentInf);
+            if (vm.currentInf.index === vm.diary.length) {
+                vm.diary.push(vm.currentInf);
             } else {
                 vm.diary[vm.currentInf.index] = vm.currentInf;
-            } 
+            }
         }
+
         function init() {
+
+            /*// Simple GET request example:
+            $http({
+                method: 'GET'
+                , url: '/someUrl'
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                vm.diary = response.data;
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });*/
+           
             vm.diary = [
-                { index:0, site: 'vk', login: 'mylogin', pass: 'mypass' },
-                {  index:1, site: 'inst', login: '1login', pass: '1pass' },
-            ];
+                {
+                    index: 0
+                    , site: 'vk'
+                    , login: 'mylogin'
+                    , pass: '*****'
+                }
+                , {
+                    index: 1
+                    , site: 'inst'
+                    , login: '1login'
+                    , pass: '****'
+                    
+                }
+            , ];
         }
-         function add() {
+
+        function add() {
             console.log("Button add clicked");
-            vm.currentInf = { index:vm.diary.length, site: '', login: '', pass: '' };
+            vm.currentInf = {
+                index: vm.diary.length
+                , site: ''
+                , login: ''
+                , pass: ''
+            };
         }
-         function edit(row) {
+
+        function edit(row) {
             console.log("Row was clicked", row);
             vm.currentInf = angular.copy(row);
         }
+
         function sort(fieldName) {
             if (vm.sortField === fieldName) {
                 vm.reverse = !vm.reverse;
@@ -49,5 +82,6 @@
                 vm.reverse = false;
             }
         }
-         
-}})();
+        
+    }
+})();
